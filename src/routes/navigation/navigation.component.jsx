93a35507aaa93bd6a49a32a47deeac0552537958
Abel Parayil Abraham <abelabr321@gmail.com>
2024-08-12 10:05:40 +0530

    netlify bugs

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,22 +1,22 @@
-import { Fragment, useContext } from "react";
-import { Outlet, Link } from "react-router-dom";
-import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import { Fragment, useContext } from 'react';
+import { Outlet } from 'react-router-dom';
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 
-import { UserContext } from "../../contexts/user.context";
-import { CartContext } from "../../contexts/cart.context";
+import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
 
-import { signOutUser } from "../../utils/firebase/firebase.utils";
+import { signOutUser } from '../../utils/firebase/firebase.utils';
 
-import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartIcon from '../../components/cart-icon/cart-icon.component';
 
-import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
 import {
   NavigationContainer,
   NavLink,
   NavLinks,
   LogoContainer,
-} from "./navigation.styles";
+} from './navigation.styles';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,14 +1,13 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import {
   getAuth,
-  signInWithRedirect,
   signInWithPopup,
   createUserWithEmailAndPassword,
   GoogleAuthProvider,
   signInWithEmailAndPassword,
   signOut,
   onAuthStateChanged,
-} from "firebase/auth";
+} from 'firebase/auth';
 
 import {
   getFirestore,
@@ -19,25 +18,24 @@ import {
   writeBatch,
   query,
   getDocs,
-} from "firebase/firestore";
+} from 'firebase/firestore';
 
 /*--------------------- Firebase configurations -------------------------- */
 const firebaseConfig = {
-  apiKey: "AIzaSyD8syg4pdQL6jMhW9EaKNV0KzFq6ih7qx8",
-  authDomain: "cloth-store-db-a1e71.firebaseapp.com",
-  projectId: "cloth-store-db-a1e71",
-  storageBucket: "cloth-store-db-a1e71.appspot.com",
-  messagingSenderId: "436604764361",
-  appId: "1:436604764361:web:2a451c6b69a85fd8068d18",
+  apiKey: 'AIzaSyD8syg4pdQL6jMhW9EaKNV0KzFq6ih7qx8',
+  authDomain: 'cloth-store-db-a1e71.firebaseapp.com',
+  projectId: 'cloth-store-db-a1e71',
+  storageBucket: 'cloth-store-db-a1e71.appspot.com',
+  messagingSenderId: '436604764361',
+  appId: '1:436604764361:web:2a451c6b69a85fd8068d18',
 };
 
-/*------------- Initialised with Firebase configurations ----------------- */
-const firebaseApp = initializeApp(firebaseConfig);
+// const firebaseApp = initializeApp(firebaseConfig);
 
 /*------------- Google auth provider ----------------- */
 const googleProvider = new GoogleAuthProvider();
 googleProvider.setCustomParameters({
-  prompt: "select_account",
+  prompt: 'select_account',
 });
 
 export const auth = getAuth();
@@ -61,11 +59,11 @@ export const addCollectionAndDocuments = async (
   });
 
   await batch.commit();
-  console.log("done");
+  console.log('done');
 };
 
 export const getCategoriesAndDocuments = async () => {
-  const collectionRef = collection(db, "categories");
+  const collectionRef = collection(db, 'categories');
   const q = query(collectionRef);
 
   const querySnapshot = await getDocs(q);
@@ -82,7 +80,7 @@ export const createUserDocumentFromAuth = async (
   userAuth,
   additionalInformation
 ) => {
-  const userDocRef = doc(db, "users", userAuth.uid);
+  const userDocRef = doc(db, 'users', userAuth.uid);
   // console.log(userDocRef);
 
   const userSnapshot = await getDoc(userDocRef);
